34552bff8bef820d7dc4c3d0d1e7f3628f1228a4
User <User@DESKTOP-TJ0K7RR>
2024-05-14 19:37:59 +0300

    Добавьте файлы проекта.

---
+++ Указатели/Указатели.cpp
@@ -0,0 +1,55 @@
+﻿// Указатели.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+//Морской бой
+
+//setlocale(LC_ALL, "Russian");
+//    SetConsoleCP(1251);
+//    SetConsoleOutputCP(1251);
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+   
+    cout << "Добрый день,вас приветствует разработчик Кожетьева Маргарита Вячеславовна\n\n";
+   
+    return 0;
+}
+
+
+
+
+
+
+    
+
+
+
+
+    
+
+
+
+
+    
+   
+  
+
+ 
+
+
+
